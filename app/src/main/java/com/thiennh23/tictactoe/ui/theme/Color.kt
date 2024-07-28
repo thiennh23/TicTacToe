9a843f43c90ffa1ba7d32bbf3d2632079971934c
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-07-28 18:12:47 +0700

    Initial commit

---
+++ app/src/main/java/com/thiennh23/tictactoe/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.thiennh23.tictactoe.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Aqua = Color(0xff04D4F4)
+val GreenishYellow = Color(0xffa8c920)
+val GrayBackground = Color(0xfff5f7f6)
+val BlueCustom = Color(0xff3871df)
